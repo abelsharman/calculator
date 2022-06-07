9f5971acb3c072bac24f1dbfb1371e8fe561b1a4
Tlek Suyerbassov <tleksuerbasov@mail.ru>
2022-06-07 19:37:48 +0600

    new: style of blocks

---
+++ index.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        result: 689123, 
+        expression: [],
+    },
+    methods: {
+        cancel(){
+
+        }
+    },
+    created(){
+        console.log(1);
+    }
+})
